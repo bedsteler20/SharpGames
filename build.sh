1b932d10776abe0f49183de80b5047d8972897c8
Cameron Dehning <bedsteler2.0@gmail.com>
2023-09-29 17:53:11 -0500

    edit build files

--- build.sh
+++ build.sh
@@ -7,12 +7,3 @@ flatpak-builder --user --install --force-clean \
                 --state-dir=obj/Flatpak/State  \
                 obj/Flatpak/Build              \
                 dev.bedsteler20.SharpGames.yaml
-                
-rm -rf ./obj/AppImage/AppDir || true
-cp -r bin/Release/net7.0/linux-x64 ./obj/AppImage/AppDir
-mkdir -p obj/AppImage/Build
-
-appimage-builder --appdir=obj/AppImage/AppDir \
-                 --build-dir=obj/AppImage/Build
-
-mv *.AppImage bin/
